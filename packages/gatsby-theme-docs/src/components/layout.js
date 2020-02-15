af11331ef383c98a057d6e6a402fd4149d2cc89f
Aaron Thompson <thompson_ad@outlook.com>
2020-02-15 17:24:07 +0000

    add theme ui

--- packages/gatsby-theme-docs/src/components/layout.js
+++ packages/gatsby-theme-docs/src/components/layout.js
@@ -1,10 +1,17 @@
 /** @jsx jsx */
 import { Fragment } from 'react';
+import { jsx } from 'theme-ui';
+import { Global } from '@emotion/core';
 
 const Layout = ({ children }) => (
   <Fragment>
-    <header>gatsby-theme-docs</header>
-    <main>{children}</main>
+    <Global styles={{ body: { margin: 0 } }} />
+    <header
+      sx={{ bg: 'primary', color: 'background', fontFamily: 'heading', p: 3 }}
+    >
+      gatsby-theme-docs
+    </header>
+    <main sx={{ mx: 'auto', maxWidth: 650, width: '90vw' }}>{children}</main>
   </Fragment>
 );
 
